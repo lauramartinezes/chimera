8e4ddfced39bc67b739b48c232a2852d8ca5b94e
u0159868 <u0159868@kuleuven.be>
2024-07-25 23:30:38 +0200

    test

--- kMRCD_python/example.py
+++ kMRCD_python/example.py
@@ -8,7 +8,7 @@ from standarization import rz_scores
 from scipy.io import savemat
 
 def main():
-    np.random.seed(5)
+    np.random.seed(5) #test
     
     color_BLUE = (0, 0.6980, 0.9333)
     color_ORANGE = (0.9333, 0.4627, 0)
