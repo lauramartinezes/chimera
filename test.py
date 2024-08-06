fdb59218e9691e752f88e2255f9988c1af36d49f
u0159868 <u0159868@kuleuven.be>
2024-08-06 12:14:27 +0200

    allow test to stand by itself and remove unsused code

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 
 import numpy as np
@@ -5,7 +6,8 @@ import pandas as pd
 import timm
 import torch
 
-from torch import nn, optim
+from torch import optim
+from config.config import SAVE_DIR
 from datasets.load_data import *
 from datasets.transforms import set_test_transform, set_train_transform
 from model.test import test_model
@@ -18,14 +20,19 @@ batch_size = 32
 batch_size_val = 32
 num_workers = 0
 
+def get_args():
+    parser = argparse.ArgumentParser(description='Test a separate iteration')
+    parser.add_argument('--iteration', type=int, default=0)
+    args = parser.parse_args()
+    return args
 
 def test(model_path, df_train, df_val, df_test, save_folder):
     # Get dataloader
     datasets = get_datasets(df_train, df_val, df_test, transform_train=set_train_transform(False), transform_test=set_test_transform())
-    train_dataset, valid_dataset, test_dataset = datasets.values()
+    _, _, test_dataset = datasets.values()
 
     dataloaders = load_data(datasets, batch_size, batch_size_val, num_workers)
-    train_dataloader, valid_dataloader, test_dataloader = dataloaders.values()
+    _, _, test_dataloader = dataloaders.values()
     topclasses = df_train['txt_label'].unique().tolist()
 
     model = timm.create_model(architecture, pretrained=True, num_classes=len(topclasses))
@@ -35,10 +42,6 @@ def test(model_path, df_train, df_val, df_test, save_folder):
     print(f'Train on gpu: {train_on_gpu}')  # Number of gpus
     model = model.to('cuda', dtype=torch.float)
 
-    class_sample_count = np.unique(df_train.label, return_counts=True)[1]
-    weight = 1. / class_sample_count
-    criterion = nn.CrossEntropyLoss(
-        label_smoothing=.15, weight=torch.Tensor(weight).cuda())
     optimizer = optim.AdamW(model.parameters(), lr=.003, weight_decay=0.01)
     model, optimizer = load_checkpoint(model_path, model, optimizer)
     model = model.to('cuda', dtype=torch.float)
@@ -48,3 +51,16 @@ def test(model_path, df_train, df_val, df_test, save_folder):
     insect_accuracies.to_csv(os.path.join(save_folder, f'accuracy_values.npy'), index=False)
 
     return bacc, cm, y_true, y_pred, info, insect_accuracies
+
+if __name__ == '__main__':
+    args = get_args()
+    i = args.iteration 
+    saved_model = 'fuji_tf_efficientnetv2_m.in21k_ft_in1k_pretrained_imagenet_None_seed_x_augmented'
+    iteration_folder = os.path.join('output', f'iteration_{i}')
+    model_i_path = os.path.join(iteration_folder, f'{saved_model}_{i}_best.pth.tar')
+
+    df_train = pd.read_parquet(f"{SAVE_DIR}/df_train_{setting}.parquet") # the train is not precise but it is not really used in test so whatever
+    df_val = pd.read_parquet(f"{SAVE_DIR}/df_val_{setting}.parquet")
+    df_test = pd.read_parquet(f"{SAVE_DIR}/df_test_{setting}.parquet")
+
+    test(model_i_path, df_train, df_val, df_test, iteration_folder)
