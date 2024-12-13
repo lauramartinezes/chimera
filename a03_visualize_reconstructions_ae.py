1be2fc6efdada8a06428fc54bbb81790df7c7166
u0159868 <u0159868@kuleuven.be>
2024-12-13 16:17:42 +0100

    remove unused libraries

--- a03_visualize_reconstructions_ae.py
+++ a03_visualize_reconstructions_ae.py
@@ -14,7 +14,6 @@ from tqdm import tqdm
 import umap
 import yaml
 
-from a06_outliers_evaluation import extract_features_from_encoding, visualize_latent_space
 from mnist_dataset import CustomBinaryInsectDF
 from vq_vae import VQVAE
 
