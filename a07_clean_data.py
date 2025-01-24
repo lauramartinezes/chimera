bf259ba8d48b9a098b5332c6586bb552a0211d31
u0159868 <u0159868@kuleuven.be>
2025-01-24 17:45:42 +0100

    add custom mcd contamination and printing of metrics

--- a07_clean_data.py
+++ a07_clean_data.py
@@ -59,9 +59,11 @@ def clean_df(df, model, device, config, transform, pin_memory, main_insect_class
         latents_512d = latents
 
     y_true = (measurement_noise + mislabel_noise).astype(int)
-    y_pred = get_outlier_predictions(
+    y_pred, metrics = get_outlier_predictions(
         latents_512d,
-        model='MCD'
+        y_true,
+        model='MCD',
+        contamination= 0.21 if main_insect_class == 'c' else 0.11
     )
 
     visualize_y_true_vs_y_pred_umap(
@@ -74,7 +76,9 @@ def clean_df(df, model, device, config, transform, pin_memory, main_insect_class
     )
 
     df_clean = df[y_pred == 0]
-    return df_clean
+    if f'mislabeled_{main_insect_class}' in df_clean.columns:
+        df_clean = df_clean[df_clean[f'mislabeled_{main_insect_class}'] == False]
+    return df_clean, metrics
 
 
 def load_data_from_df(df, transform, seed, batch_size, num_workers, pin_memory):
@@ -124,14 +128,15 @@ def extract_features_from_encoding(model, dataloader, device):
 
 
 
-def get_outlier_predictions(X_train, model='HBOS'):
+def get_outlier_predictions(X_train, y_train, model='MCD', contamination=0.1):
     print(f'{model} outlier extraction')
-    pyod_model = PYOD(seed=42, model_name=model)
+    pyod_model = PYOD(seed=42, model_name=model, contamination=contamination)
     pyod_model.fit(X_train, [])
     anomaly_scores = pyod_model.predict_score(X_train)
+    metrics = metric(y_true=y_train, y_score=anomaly_scores, pos_label=1)
     anomaly_binary_scores = pyod_model.predict_binary_score(X_train)
 
-    return anomaly_binary_scores
+    return anomaly_binary_scores, metrics
 
 
 def visualize_y_true_vs_y_pred_umap(features, measurement_noises, label_noises, y_pred, filename=None, dirname=None):
@@ -219,10 +224,11 @@ if __name__ == '__main__':
             model_ae.to(device)
             print("Model correctly initialized")
 
-            df_train_clean = clean_df(df_train, model_ae, device, config, transform_ae, pin_memory, main_insect_class, phase="train", method=f'{ae_type}ae')
+            df_train_clean, metrics = clean_df(df_train, model_ae, device, config, transform_ae, pin_memory, main_insect_class, phase="train", method=f'{ae_type}ae')
             df_train_clean_path = os.path.join('data', f'df_train_{ae_type}ae_{main_insect_class}_clean.csv')
             df_train_clean.to_csv(df_train_clean_path, index=False)
 
-            print(f'Clean {main_insect_class} training dataset available')
+            print(f'Clean {main_insect_class} {ae_type} training dataset available')
+            print('metrics: ', metrics)
 
             print('')
