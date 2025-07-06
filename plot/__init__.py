401af55fedb56661a77290e7e6b57de37fd64584
u0159868 <u0159868@kuleuven.be>
2025-07-06 15:14:11 +0200

    delete legacy plot pkg

--- plot/__init__.py
+++
@@ -1,5 +0,0 @@
-import matplotlib.pyplot as plt
-from .plot_accuracies_critic_insects import plot_accuracies_critic_insects
-from .plot_accuracies import plot_accuracies_over_weeks
-
-plt.rcParams['svg.fonttype'] = 'none'
