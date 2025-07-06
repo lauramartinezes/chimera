78ab2450079d00d605b827fc36d03beaf044c997
u0159868 <u0159868@kuleuven.be>
2025-07-06 08:44:18 +0200

    move get raw df functions to datasets pkg

--- a01_prepare_dataset_raw_dfs.py
+++ a01_prepare_dataset_raw_dfs.py
@@ -1,66 +1,11 @@
-import glob
 import os
 import random
 
 import numpy as np
-import pandas as pd
 import torch
 import yaml
 
-def get_df_subset(data_dir, subset, main_insect_class, mislabeled_insect_class):
-    sample_configs = [
-        {
-            'folder': f'{main_insect_class}_good',
-            'label': 0,
-            'mislabeled': False,
-            'measurement_noise': False
-        },
-        {
-            'folder': f'{main_insect_class}_trash',
-            'label': 1,
-            'mislabeled': False,
-            'measurement_noise': True
-        },
-        {
-            'folder': f'{mislabeled_insect_class}_for_{main_insect_class}',
-            'label': 1,
-            'mislabeled': True,
-            'measurement_noise': False
-        }
-    ]
-
-    df_list = []
-
-    for config in sample_configs:
-        folder_path = os.path.join(data_dir, subset, main_insect_class, config['folder'])
-        filepaths = glob.glob(os.path.join(folder_path, '*.png'))
-
-        df = pd.DataFrame({
-            'label': config['label'],
-            'mislabeled': config['mislabeled'],
-            'measurement_noise': config['measurement_noise'],
-            'filepath': filepaths
-        })
-        df_list.append(df)
-
-    return pd.concat(df_list, ignore_index=True)
-
-
-def get_df_test(classes, data_dir):
-    df_classes_test = []
-    for i, cls in enumerate(classes):
-        class_folder_test = os.path.join(data_dir, 'test', cls)
-        class_files_test = glob.glob(os.path.join(class_folder_test, '*.png'))
-
-        df_class_test = pd.DataFrame({
-            'label': i,
-            'mislabeled': False,
-            'measurement_noise': False, 
-            'filepath': class_files_test
-        })
-        df_classes_test.append(df_class_test)
-
-    return pd.concat(df_classes_test, ignore_index=True)
+from datasets.get_raw_dfs import get_df_subset, get_df_test
 
 
 if __name__ == '__main__':
--- datasets/__init__.py
+++ datasets/__init__.py
@@ -2,3 +2,4 @@ from .datasets import CustomBinaryInsectDF
 from .load_data import load_subset_df_classification, load_data_from_df
 from .split_data import *
 from .transforms import *
+from .get_raw_dfs import get_df_subset, get_df_test
