cdcab81ad95b308e47afe255dd39e259912b7091
u0159868 <u0159868@kuleuven.be>
2025-07-06 16:19:37 +0200

    add webhook to bash script

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+WEBHOOK_URL="https://kuleuven.webhook.office.com/webhookb2/626b255b-8871-4cec-9dd6-486811dd7dfb@3973589b-9e40-4eb5-800e-b0b6383d1621/IncomingWebhook/19193a3061c54c44804b25e534c58362/bacc6a49-b24e-44a3-b54b-abc73e49688c/V2DCJVyA8y7XioAjg-m1aY_lRTnm-krjfryBo5hLOX1Ng1"
 
 files=(
   "a00_split_dataset_raw_dfs.py"
@@ -13,18 +14,37 @@ files=(
 
 for file in "${files[@]}"; do
     echo "Running $file"
-    python "$file"
+    output_file=$(mktemp)
+    python "$file" 2> >(tee -a >(grep -v '^\s*$' | grep -vi 'warning' > "$output_file") >&2)
+    # Check the exit code of the Python script
+    ERROR_MESSAGE=""
     if [ $? -ne 0 ]; then
+        OUTPUT=$(<"$output_file")
+        ERROR_MESSAGE+="ERROR: $OUTPUT"
+        PAYLOAD=$(jq -n --arg message "$ERROR_MESSAGE" '{"text": $message}')
+        curl -X POST -H "Content-Type: application/json" -d "$PAYLOAD" "$WEBHOOK_URL"
         echo "$file failed. Exiting."
         exit 1
     fi
+    rm -f "$output_file"
 done
 
 echo "Running a02_train_test_classifier.py for all cases"
-python "a02_train_test_classifier.py" --experiments all_cases
-if [ $? -ne 0 ]; then
-    echo "a02_train_test_classifier.py failed. Exiting."
-    exit 1
-fi
+output_file=$(mktemp)
+    python "a02_train_test_classifier.py" --experiments all_cases 2> >(tee -a >(grep -v '^\s*$' | grep -vi 'warning' > "$output_file") >&2)
+    # Check the exit code of the Python script
+    ERROR_MESSAGE=""
+    if [ $? -ne 0 ]; then
+        OUTPUT=$(<"$output_file")
+        ERROR_MESSAGE+="ERROR: $OUTPUT"
+        PAYLOAD=$(jq -n --arg message "$ERROR_MESSAGE" '{"text": $message}')
+        curl -X POST -H "Content-Type: application/json" -d "$PAYLOAD" "$WEBHOOK_URL"
+        echo "a02_train_test_classifier.py failed. Exiting."
+        exit 1
+    fi
 
-echo "All scripts executed successfully!"
+
+END_MESSAGE="All scripts executed successfully!"
+curl -X POST -H "Content-Type: application/json" -d "{\"text\":\"$END_MESSAGE\"}" $WEBHOOK_URL
+
+echo $END_MESSAGE
