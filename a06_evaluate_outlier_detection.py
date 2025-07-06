0dc33612dceb80379021ca1d8a1697ccb0e9a973
u0159868 <u0159868@kuleuven.be>
2025-07-06 12:27:26 +0200

    restrucutring to get rid of process data cnn

--- a06_evaluate_outlier_detection.py
+++ a06_evaluate_outlier_detection.py
@@ -14,34 +14,16 @@ from outlier_detectors import get_outlier_methods_csv
 from models import extract_features
 
 
-def process_data_cnn(df, model, device, config, transform, main_insect_class, phase="train", cnn_type='cnn'):
-    loader = load_data_from_df(
-        df,
-        transform,
-        config["exp_params"]["manual_seed"],
-        config["data_params"][f"batch_size"],
-        config["data_params"]["num_workers"],
-        pin_memory,
-        shuffle=False
-    )
-    latents_cnn, labels_cnn, real_labels_cnn, measurement_noise_cnn, mislabeled_cnn = extract_features(loader, model)
-
+def preprocess_latents_for_outlier_detection(latents_cnn, cnn_type):
     if '2d' in cnn_type:
         reducer_2_d = umap.UMAP(n_components=2, random_state=42, n_jobs=1)
-        latents_cnn = reducer_2_d.fit_transform(latents_cnn)
+        return reducer_2_d.fit_transform(latents_cnn)
 
-    if 'napoletano' in cnn_type:
+    elif 'napoletano' in cnn_type:
         pca = PCA(n_components=0.95)
-        latents_cnn = pca.fit_transform(latents_cnn)
-
-    get_outlier_methods_csv(
-        latents_cnn,
-        measurement_noise_cnn.astype(int),
-        mislabeled_cnn.astype(int),
-        f'{cnn_type}_512d_{main_insect_class}_{phase}',
-        dirname=config["logging_params"]["save_dir"]
-    )
-
+        return pca.fit_transform(latents_cnn)
+    else:
+        return latents_cnn
 
 if __name__ == '__main__':
     # Load the configuration
@@ -56,94 +38,51 @@ if __name__ == '__main__':
     pin_memory = len(config['trainer_params']['gpus']) != 0
     data_dir = config["data_params"]["splitted_data_dir"]
     insect_classes = config["data_params"]["data_classes"]
-    model_name = config["model_params"]["name"]
 
     df_test_path = os.path.join(data_dir, f'df_test.csv')
     df_test = pd.read_csv(df_test_path)
 
-    transform_cnn = set_feature_extraction_transform()
-
-    device = config["trainer_params"]["device"]
-    raw_suffix = config["data_params"]["raw_suffix"]
-    swap_suffix = config["data_params"]["swap_suffix"]
+    cnn_types = ['cnn', 'cnn_2d', 'adbench', 'adbench_2d', 'adbench_napoletano']
     
-    for i in range(len(insect_classes)):
-        main_insect_class = insect_classes[i]
+    for i, main_insect_class in enumerate(insect_classes):
         mislabeled_insect_class = insect_classes[1 - i]
 
-        model_cnn = timm.create_model(model_name, pretrained=config["model_params"]["pretrained"])
+        model_cnn = timm.create_model(config["model_params"]["name"], pretrained=config["model_params"]["pretrained"])
         model_cnn = torch.nn.Sequential(*(list(model_cnn.children())[:-1]))
         model_cnn.eval()
 
-        # Resnet method
-        df_train_path = os.path.join(data_dir, f'df_train_{swap_suffix}_{main_insect_class}.csv')
-        df_val_path = os.path.join(data_dir, f'df_val_{swap_suffix}_{main_insect_class}.csv')
-        df_train_ = pd.read_csv(df_train_path)
-        df_val_ = pd.read_csv(df_val_path)
-        df_train = pd.concat([df_train_, df_val_], ignore_index=True)       
+        for cnn_type in cnn_types:
+            if 'adbench' in cnn_type:
+                suffix = config["data_params"]["raw_suffix"]
+            else:
+                suffix = config["data_params"]["swap_suffix"]
+            
+            df_train_path = os.path.join(data_dir, f'df_train_{suffix}_{main_insect_class}.csv')
+            df_val_path = os.path.join(data_dir, f'df_val_{suffix}_{main_insect_class}.csv')
+            df_train_ = pd.read_csv(df_train_path)
+            df_val_ = pd.read_csv(df_val_path)
+            df_train = pd.concat([df_train_, df_val_], ignore_index=True)   
 
-        process_data_cnn(
-            df_train, 
-            model_cnn, 
-            device, 
-            config, 
-            transform_cnn, 
-            main_insect_class, 
-            phase="train", 
-            cnn_type='cnn_2d'
-        )   
+            loader = load_data_from_df(
+                df_train,
+                set_feature_extraction_transform(),
+                config["exp_params"]["manual_seed"],
+                config["data_params"][f"batch_size"],
+                config["data_params"]["num_workers"],
+                pin_memory,
+                shuffle=False
+            )
+            latents_cnn, labels_cnn, real_labels_cnn, measurement_noise_cnn, mislabeled_cnn = extract_features(loader, model_cnn) 
 
-        process_data_cnn(
-            df_train, 
-            model_cnn, 
-            device, 
-            config, 
-            transform_cnn, 
-            main_insect_class, 
-            phase="train", 
-            cnn_type='cnn'
-        )  
+            latents_cnn = preprocess_latents_for_outlier_detection(latents_cnn, cnn_type)
 
-        print('Metrics for CNN are available') 
+            get_outlier_methods_csv(
+                latents_cnn,
+                measurement_noise_cnn.astype(int),
+                mislabeled_cnn.astype(int),
+                f'{cnn_type}_512d_{main_insect_class}_train',
+                dirname=config["logging_params"]["save_dir"]
+            )
+ 
+            print(f'Metrics for {cnn_type} are available')  
 
-        # Adbench method
-        df_train_path = os.path.join(data_dir, f'df_train_{raw_suffix}_{main_insect_class}.csv') 
-        df_val_path = os.path.join(data_dir, f'df_val_{raw_suffix}_{main_insect_class}.csv')
-        df_train_ = pd.read_csv(df_train_path)
-        df_val_ = pd.read_csv(df_val_path)
-        df_train = pd.concat([df_train_, df_val_], ignore_index=True)
-
-        process_data_cnn(
-            df_train, 
-            model_cnn, 
-            device, 
-            config, 
-            transform_cnn, 
-            main_insect_class, 
-            phase="train", 
-            cnn_type='adbench_2d'
-        ) 
-
-        process_data_cnn(
-            df_train, 
-            model_cnn, 
-            device, 
-            config, 
-            transform_cnn, 
-            main_insect_class, 
-            phase="train", 
-            cnn_type='adbench'
-        )  
-
-        process_data_cnn(
-            df_train, 
-            model_cnn, 
-            device, 
-            config, 
-            transform_cnn, 
-            main_insect_class, 
-            phase="train", 
-            cnn_type='adbench_napoletano'
-        )  
-
-        print('Metrics for AdBench are available')
