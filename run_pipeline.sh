fa78e36c46447633239887c6e76a9c3efaa16e75
u0159868 <u0159868@kuleuven.be>
2025-07-22 15:39:51 +0200

    fix bug scripts running in disorder

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-declare -A files_with_args=(
-  ["a00_split_dataset_raw_dfs.py"]=""
-  ["a01_prepare_dataset_raw_dfs.py"]=""
-  ["a02_train_test_classifier.py"]=""
-  ["a03_prepare_data_swap_dfs.py"]="" 
-  ["a04_generate_umaps.py"]=""
-  ["a06_evaluate_outlier_detection.py"]=""
-  ["a07_clean_dfs.py"]="" 
-  ["a08_analyze_meas_label_noise.py"]="" 
-  ["a02_train_test_classifier.py --experiments all_cases"]=""
+files_with_args=(
+  "a00_split_dataset_raw_dfs.py"
+  "a01_prepare_dataset_raw_dfs.py"
+  "a02_train_test_classifier.py"
+  "a03_prepare_data_swap_dfs.py"
+  "a04_generate_umaps.py"
+  "a06_evaluate_outlier_detection.py"
+  "a07_clean_dfs.py"
+  "a08_analyze_meas_label_noise.py"
+  "a02_train_test_classifier.py --experiments all_cases"
 )
 
-for entry in "${!files_with_args[@]}"; do
+for entry in "${files_with_args[@]}"; do
     file="${entry%% *}"
     args="${entry#"$file"}"
 
