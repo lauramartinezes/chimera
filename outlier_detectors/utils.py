69e69b5ca8a73e342888366f90bb74b5620c2178
u0159868 <u0159868@kuleuven.be>
2025-12-22 09:49:11 +0100

    remove napoletano option

--- outlier_detectors/utils.py
+++ outlier_detectors/utils.py
@@ -3,7 +3,6 @@ import numpy as np
 import torch
 import umap
 
-from sklearn.decomposition import PCA
 from sklearn.metrics import roc_auc_score, average_precision_score
 
 
@@ -29,9 +28,5 @@ def preprocess_latents_for_outlier_detection(latents_cnn, cnn_type):
     if '2d' in cnn_type:
         reducer_2_d = umap.UMAP(n_components=2, random_state=42, n_jobs=1)
         return reducer_2_d.fit_transform(latents_cnn)
-
-    elif 'napoletano' in cnn_type:
-        pca = PCA(n_components=0.95)
-        return pca.fit_transform(latents_cnn)
     else:
         return latents_cnn
