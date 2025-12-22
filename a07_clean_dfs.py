9a862b5004c7c90022a474c4a91ffcb59714a119
u0159868 <u0159868@kuleuven.be>
2025-12-22 13:53:55 +0100

    remove unnecesary for loop, rename variables

--- a07_clean_dfs.py
+++ a07_clean_dfs.py
@@ -23,9 +23,8 @@ if __name__ == '__main__':
     insect_classes = config["data_params"]["data_classes"]
     data_dir = config["data_params"]["splitted_data_dir"]
     
-    # TODO check that all cases here are relevant
-    cnn_types = ['cnn', 'cnn_corrected_mislabels', 'cnn_no_od',
-                 'adbench', 'adbench_2d', 'adbench_xd_hdbscan', 'cnn_no_noise']
+    cleaning_strategies = ['cnn_corrected_mislabels', 'cnn_no_noise', 'cnn_no_od', 'cnn'
+                 'adbench', 'adbench_2d', 'adbench_xd_hdbscan']
     subsets = ['train', 'val']
 
     results = []
@@ -39,77 +38,74 @@ if __name__ == '__main__':
     os.makedirs(os.path.join(data_dir, 'clean'), exist_ok=True)
     os.makedirs(os.path.join(data_dir, 'outliers'), exist_ok=True)
 
-    for cnn_type in cnn_types:
+    for strategy in cleaning_strategies:
         for i, main_insect_class in enumerate(insect_classes):
             mislabeled_insect_class = insect_classes[1 - i]
 
             df_subsets = []
             for subset in subsets:
-                if 'adbench' in cnn_type:
+                if 'adbench' in strategy:
                     df_subset_path = os.path.join(data_dir, f'df_{subset}_{raw_suffix}_{main_insect_class}.csv')
                 else:
                     df_subset_path = os.path.join(data_dir, f'df_{subset}_{swap_suffix}_{main_insect_class}.csv')
                 df_subset = pd.read_csv(df_subset_path)
-                if cnn_type == 'cnn_no_noise':
+                if strategy == 'cnn_no_noise':
                     df_subset = df_subset[(df_subset['mislabeled'] == False) & (df_subset['measurement_noise'] == False)]
                 df_subsets.append(df_subset)
             df_train_val = pd.concat(df_subsets, ignore_index=True)
 
-            if 'cnn' in cnn_type and 'no_od' not in cnn_type:
-                od_methods = ['UmapHdbscanOD']  
-            elif cnn_type == 'adbench':
-                od_methods = ['ECOD']
-            elif 'adbench_2d' in cnn_type:
-                od_methods = ['MCD']
-            elif cnn_type == 'adbench_xd_hdbscan':
-                od_methods = ['UmapHdbscanOD']
-            elif cnn_type == 'cnn_no_od':
-                od_methods = ['no_od']
+            if ('cnn' in strategy and 'no_od' not in strategy) or (strategy == 'adbench_xd_hdbscan'):
+                od_method = 'UmapHdbscanOD'
+            elif strategy == 'adbench':
+                od_method = 'ECOD'
+            elif 'adbench_2d' in strategy:
+                od_method = 'MCD'
+            elif strategy == 'cnn_no_od':
+                od_method = 'no_od'
             
-            for od_method in od_methods:
-                if od_method == 'no_od':
-                    df_train_val_clean, df_train_val_outliers, metrics = clean_df_no_od(df_train_val, main_insect_class)
-                else:
-                    df_train_val_clean, df_train_val_outliers, metrics = clean_df(
-                        df_train_val, 
-                        model, 
-                        config, 
-                        set_feature_extraction_transform(), 
-                        pin_memory, 
-                        main_insect_class, 
-                        phase='train_val', 
-                        method=cnn_type, 
-                        od_method=od_method
-                    )
-                
-                for subset in subsets:
-                    df_subset_clean = df_train_val_clean[df_train_val_clean.filepath.str.contains(subset)]
-                    df_subset_outliers = df_train_val_outliers[df_train_val_outliers.filepath.str.contains(subset)]
+            if od_method == 'no_od':
+                df_train_val_clean, df_train_val_outliers, metrics = clean_df_no_od(df_train_val, main_insect_class)
+            else:
+                df_train_val_clean, df_train_val_outliers, metrics = clean_df(
+                    df_train_val, 
+                    model, 
+                    config, 
+                    set_feature_extraction_transform(), 
+                    pin_memory, 
+                    main_insect_class, 
+                    phase='train_val', 
+                    method=strategy, 
+                    od_method=od_method
+                )
+            
+            for subset in subsets:
+                df_subset_clean = df_train_val_clean[df_train_val_clean.filepath.str.contains(subset)]
+                df_subset_outliers = df_train_val_outliers[df_train_val_outliers.filepath.str.contains(subset)]
 
-                    df_subset_clean_path = os.path.join(
-                        data_dir, 
-                        'clean', 
-                        f'df_{subset}_{cnn_type}_{main_insect_class}_{od_method}_clean.csv'
-                    )
-                    df_subset_clean.to_csv(df_subset_clean_path, index=False)
-                    print(f'Clean {main_insect_class} {cnn_type} {subset} dataset available')
+                df_subset_clean_path = os.path.join(
+                    data_dir, 
+                    'clean', 
+                    f'df_{subset}_{strategy}_{main_insect_class}_{od_method}_clean.csv'
+                )
+                df_subset_clean.to_csv(df_subset_clean_path, index=False)
+                print(f'Clean {main_insect_class} {strategy} {subset} dataset available')
 
-                    df_subset_outliers_path = os.path.join(
-                        data_dir, 
-                        'outliers', 
-                        f'df_{subset}_{cnn_type}_{main_insect_class}_{od_method}_outliers.csv'
-                    )
-                    df_subset_outliers.to_csv(df_subset_outliers_path, index=False)
-                    print(f'Outliers {main_insect_class} {cnn_type} {subset} dataset available')
+                df_subset_outliers_path = os.path.join(
+                    data_dir, 
+                    'outliers', 
+                    f'df_{subset}_{strategy}_{main_insect_class}_{od_method}_outliers.csv'
+                )
+                df_subset_outliers.to_csv(df_subset_outliers_path, index=False)
+                print(f'Outliers {main_insect_class} {strategy} {subset} dataset available')
 
-                # Add more information to the metrics dictionary
-                metrics['method'] = cnn_type
-                metrics['od_method'] = od_method
-                metrics['main_insect_class'] = main_insect_class
+            # Add more information to the metrics dictionary
+            metrics['method'] = strategy
+            metrics['od_method'] = od_method
+            metrics['main_insect_class'] = main_insect_class
 
-                results.append(metrics)
-                df_results = pd.DataFrame(results)
-                results_path = os.path.join(config["logging_params"]["save_dir"], 'df_best_od_evaluation.csv')
-                df_results.to_csv(results_path, mode='a', index=False, header=False)
-                print('metrics: ', metrics)
+            results.append(metrics)
+            df_results = pd.DataFrame(results)
+            results_path = os.path.join(config["logging_params"]["save_dir"], 'df_best_od_evaluation.csv')
+            df_results.to_csv(results_path, mode='a', index=False, header=False)
+            print('metrics: ', metrics)
             print('')
